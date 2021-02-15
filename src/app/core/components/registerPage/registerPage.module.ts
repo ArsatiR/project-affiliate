30f5a4078c9b75955c1600ac93535eeecdb3322d
armandowni <armando.nurimansyah@gmail.com>
2021-02-15 22:14:09 +0700

    register page

---
+++ src/app/core/components/registerPage/registerPage.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RegisterPageComponent } from './registerPage.component';
+import { RouterModule, Routes } from '@angular/router';
+import { SharedModule } from '../../../core/modules/shared.module';
+
+const routes: Routes = [
+  {
+    path: 'register',
+    component: RegisterPageComponent,
+  }
+];
+
+@NgModule({
+  exports: [
+    RegisterPageComponent
+  ],
+  imports: [
+    CommonModule,
+    SharedModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [RegisterPageComponent]
+})
+export class RegisterPageModule { }

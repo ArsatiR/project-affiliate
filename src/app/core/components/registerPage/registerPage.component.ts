7a39ad62ee5c0230890ae51c70280bedcf7cf701
Arsati <arsajack@gmail.com>
2021-02-22 17:16:10 +0700

    coba2 animasi

--- src/app/core/components/registerPage/registerPage.component.ts
+++ src/app/core/components/registerPage/registerPage.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { slideInAnimation } from 'src/app/animations';
 
 @Component({
   selector: 'app-registerPage',
   templateUrl: './registerPage.component.html',
-  styleUrls: ['./registerPage.component.scss']
+  styleUrls: ['./registerPage.component.scss'],
+  animations:[slideInAnimation]
 })
 export class RegisterPageComponent implements OnInit {
 
